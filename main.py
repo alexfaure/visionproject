4320ee1bbe465fde0b38e693fc5ce08957978b7b
faure.alexandre <alexandre.faure42@orange.fr>
2022-12-14 14:48:45 +0100

    fct liste presque correcte + calcule de mot verif dico

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def traitement_images(im):
     thresh1 = cv2.medianBlur(thresh1, 5)
     cv2.imshow('image', thresh1)
     cv2.waitKey(0)
-    thresh1 = cv2.dilate(thresh1, np.ones((4, 4), np.uint8), iterations=1)
+    thresh1 = cv2.dilate(thresh1, np.ones((5, 5), np.uint8), iterations=1)
     cv2.imshow('image', thresh1)
     cv2.waitKey(0)
     thresh1 = cv2.erode(thresh1, np.ones((5, 5), np.uint8), iterations=1)
@@ -32,8 +32,9 @@ def traitement_images(im):
 
 
 def detection_mot(tresholedimg):
-    mots = (pytesseract.image_to_string(tresholedimg, config='--psm 11 -c tessedit_char_whitelist=ABCDEFGHIiJKL'
-                                                             'MNOPQRSTUVWXYZ'))
+    mots = (pytesseract.image_to_string(tresholedimg,
+                                        config='--psm 6 -l fra --oem 0 -c tessedit_char_whitelist=ABCDEFGHIJKL'
+                                               'MNOPQRSTUVWXYZ'))
     mots = mots.replace(' ', '')
     results = []
     for line in mots.split('\n'): results.append(line.strip())
@@ -42,17 +43,67 @@ def detection_mot(tresholedimg):
     # return [str(result) for result in results if len(result) > 1]
 
 
-def score_mot(letterlist, word):
+def score_mot(letterlist, word, dico):
     score = 0
     lst = []
-    word = [i.upper() for i in word]
-    for letters in word:
-        lst.append(letters)
-    for i in lst:
-        for j in letterlist:
-            if i == j[0]:
-                score = score + j[1]
-    return score
+    if word in dico:
+        word = [i.upper() for i in word]
+        for letters in word:
+            lst.append(letters)
+        for i in lst:
+            for j in letterlist:
+                if i == j[0]:
+                    score = score + j[1]
+        return score
+    return 0
+
+
+def delete_multiple_element(list_object, indices):
+    indices = sorted(indices, reverse=True)
+    for idx in indices:
+        if idx < len(list_object):
+            list_object.pop(idx)
+
+
+def ajoutdesindicesencommun(list_n):
+    listIndex = [[] for i in range(len(list_n))]
+    listMot = []
+    i = 0
+    for j, k in enumerate(list_n):
+
+        if len(k) == 1:
+            listIndex[i].append(j)
+            i -= 1
+        i += 1
+
+    listIndex = [x for x in listIndex if len(x) > 0]
+
+    listIndexNew = [-1 for x in listIndex]
+
+    for k, i in enumerate(listIndex):
+        mot = ''
+        for j in i:
+            mot += list_n[j]
+            if listIndexNew[k] == -1:
+                listIndexNew[k] = j
+
+        listMot.append(mot)
+
+    k = 0
+    p = 0
+    for i, j in zip(listIndexNew, listMot):
+        if list_n[i - p] != j:
+            list_n[i - p] = j
+
+            for u in range(1, len(listIndex[k])):
+                list_n.remove(list_n[listIndex[k][u] - p])
+                p += 1
+
+        print(list_n, '\n')
+        k += 1
+
+    print('list', list_n)
+    return list_n
 
 
 # Press the green button in the gutter to run the script.
@@ -64,10 +115,11 @@ if __name__ == '__main__':
     scoreB = 0
     nbTours = 0
     listWords = open(r"ressources\\ods6.txt").read().splitlines()
-    path = r'ressources\\IMG_5.jpg'
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    thresholdedImg = traitement_images(img)
-    resultList = detection_mot(thresholdedImg)
-    print(resultList)
-    mot = str(resultList)
-
+    for i in range(10, 17):
+        path = fr'ressources\\IMG_2_{str(i)}.jpg'
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        thresholdedImg = traitement_images(img)
+        resultList = detection_mot(thresholdedImg)
+        print("result: ", resultList)
+        res = ajoutdesindicesencommun(resultList)
+        print(res)
