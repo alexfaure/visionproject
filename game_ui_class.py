ed86a0d3bd776984d4a955ac130ecd91db8c5bfc
Ziperonnn <ziperon40160@gmail.com>
2023-01-07 13:59:38 +0100

    connect interface to the data treatment

---
+++ game_ui_class.py
@@ -0,0 +1,208 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import os
+from game_logic import *
+
+
+class image(object):
+    def setup_image(self):
+
+        self.dir = "./ressources/"  # Dossier ou se trouve les images
+        list_file = os.listdir(self.dir)
+        filename = []
+        for file in list_file:
+            if file.endswith('.jpg'):
+                # image=Image.open(dir+file)
+                filename.append(self.dir + file)
+        self.n = 0
+        self.filename = filename.copy()
+        self.file = self.filename[self.n]
+
+    def next_image(self):
+        self.n += 1
+        self.file = self.filename[self.n]
+
+
+class joueur(object):
+    def __init__(self):
+        self.name = []
+        self.score = []
+        self.total = []
+
+    def Total(self):
+        self.total = sum(self.score)
+
+    def naming(self, name):
+        self.name = name
+
+    def add_score(self, point):
+        self.score.append(point)
+        self.Total()
+
+
+class Tableau_score(object):
+    def __init__(self):
+        self.nb_joueur = 2
+        self.jactif = 0
+        self.joueurs = [joueur()] * self.nb_joueur
+
+    def iniTab(self, nb_joueur):
+        self.nb_joueur = nb_joueur
+        self.joueurs = []
+        for i in range(self.nb_joueur):
+            jt = joueur()
+            jt.name = "j" + str(i + 1)
+            self.joueurs.append(jt)
+        # test initialisation
+
+    def fin_tab(self):
+        i = 1
+        for j in self.joueurs:
+            j.name = "j" + str(i)
+            for s in range(15):
+                j.add_score(s * i)
+            j.Total()
+            i += 1
+
+    def next(self):
+        # Permet d'ajouter le score calculer et de passer à l'autre joueurs
+        self.joueurs[self.jactif].add_score(5)
+        self.jactif = (self.jactif + 1) % self.nb_joueur  # ajouter le modulo nb joueur
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        im = image()
+        im.setup_image()
+
+        self.listLetters = [['A', 1], ['B', 3], ['C', 3], ['D', 2], ['E', 1], ['F', 4], ['G', 2], ['H', 4], ['I', 1],
+                            ['J', 8], ['K', 5], ['L', 1], ['M', 3], ['N', 1], ['O', 1], ['P', 3], ['Q', 10], ['R', 1],
+                            ['S', 1], ['T', 1], ['U', 1], ['V', 4], ['W', 4], ['X', 8], ['Y', 4], ['Z', 10]]
+        self.listWords = open(r"ressources\\ods6.txt").read().splitlines()
+        self.nbTours = 0
+
+        self.Listeprecedente = []
+        self.Listenouvelle = []
+        self.listScore = []
+
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1440, 980)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(880, 690, 111, 61))
+        self.pushButton.setObjectName("Joueur suivant")
+
+        tab = Tableau_score()
+        tab.iniTab(2)
+        self.table = QtWidgets.QTableWidget(self.centralwidget)
+        self.table.setGeometry(QtCore.QRect(780, 120, 300, 300))
+        self.table.setObjectName("tableView")
+        self.table.setColumnCount(tab.nb_joueur)
+        self.table.setRowCount(len(tab.joueurs[0].score))
+        my_array = []
+        jname = []
+        for j in tab.joueurs:
+            my_array.append(j.score)
+            jname.append(j.name)
+
+        self.table.setHorizontalHeaderLabels(jname)
+        self.table.resizeColumnsToContents()
+
+        self.total = QtWidgets.QTableWidget(self.centralwidget)
+        self.total.setGeometry(QtCore.QRect(780, 120 + 300, 300, 75))
+        self.total.setObjectName("totalView")
+        self.total.setColumnCount(tab.nb_joueur)
+        self.total.setRowCount(1)
+        self.total.setHorizontalHeaderLabels(jname)
+        self.total.setVerticalHeaderLabels(["Total"])
+        self.total.resizeColumnsToContents()
+
+        self.photo = QtWidgets.QLabel(self.centralwidget)
+        self.photo.setGeometry(QtCore.QRect(20, 20, 480, 480))
+        self.photo.setText("photo")
+        self.photo.setPixmap(QtGui.QPixmap())
+        self.photo.setScaledContents(True)
+        self.photo.setObjectName("image partie")
+        # self.label = QtWidgets.QLabel(self.centralwidget)
+        # self.label.setGeometry(QtCore.QRect(20, 60, 101, 21))
+        # self.label.setObjectName("label")
+
+        # self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        # self.graphicsView.setGeometry(QtCore.QRect(400, 670, 256, 192))
+        # self.graphilcsView.setObjectName("graphicsView")
+
+        # self.textBrowser.setObjectName("textBrowser")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1102, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        print(im.filename)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        MainWindow.show()
+        self.pushButton.clicked.connect(lambda: self.show_next(im, tab))
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Joueur suivant"))
+        # self.label.setText(_translate("MainWindow", "Image de la partie"))
+
+    def show_next(self, im, tab):
+        # Début du traitement de l'image
+        img_ = cv2.imread(im.file, cv2.IMREAD_GRAYSCALE)
+        print('imhere2', type(img_))
+        thresholdedImg_ = traitement_images(img_)
+        print('imhere3')
+        result_List = detection_mot(thresholdedImg_)
+        print("result: ", result_List)
+        res, resIndex_ = ajoutdesindicesencommun(result_List)
+        for i in resIndex_:
+            if i == 0:
+                res[i] = demand_letter_after(res[i], res[i + 1])
+            elif i == len(res) - 1:
+                res[i] = demand_letter_before(res[i], res[i - 1])
+            else:
+                res[i] = demand_letter_both(res[i], res[i - 1], res[i + 1])
+        print(res)
+        for j in res:
+            score_ = score_mot(self.listLetters, j, self.listWords)
+            self.listScore.append(score_)
+            print("mot: ", j, "score: ", score_)
+
+        print('old list l181', self.Listeprecedente)
+        if self.nbTours > 0:
+            self.Listenouvelle = [ele for ele in res]
+            for a in self.Listeprecedente:
+                if a in res:
+                    self.Listenouvelle.remove(a)
+        self.Listeprecedente = res
+        print('new word in the list l190', self.Listenouvelle)
+        print('old list l190', self.Listeprecedente)
+
+        if self.nbTours < 1:
+            score = score_mot(self.listLetters, self.Listeprecedente[0], self.listWords)
+        else:
+            score = score_mot(self.listLetters, self.Listenouvelle[0], self.listWords)
+        # ajouter score au tableau pour faire le total des points
+
+        self.photo.setPixmap(QtGui.QPixmap(im.file))
+        j = tab.jactif
+        tab.next()
+        i = len(tab.joueurs[j].score) - 1
+        # print(j, i)
+        self.table.setRowCount(len(tab.joueurs[0].score))
+        self.table.setItem(i, j, QtWidgets.QTableWidgetItem(str(tab.joueurs[j].score[i])))
+        self.total.setItem(0, j, QtWidgets.QTableWidgetItem(str(tab.joueurs[j].total)))
+
+        self.table.resizeColumnsToContents()
+        self.total.resizeColumnsToContents()
+        self.nbTours += 1
+        # print(tab.joueurs[j].score)
+        im.next_image()
--- main.py
+++ main.py
@@ -1,179 +1,22 @@
-# This is a sample Python script.
-import cv2
-import numpy as np
-import pytesseract
-from pytesseract import Output
+# -*- coding: utf-8 -*-
 
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+# Form implementation generated from reading ui file 'IHM_principale.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
 
+from game_ui_class import *
 
-def traitement_images(im):
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
-    ret, thresh1 = cv2.threshold(img, 40, 255, cv2.THRESH_BINARY)
-    cv2.imshow('image', thresh1)
-    cv2.waitKey(0)
-    thresh1 = cv2.medianBlur(thresh1, 5)
-    cv2.imshow('image', thresh1)
-    cv2.waitKey(0)
-    thresh1 = cv2.dilate(thresh1, np.ones((5, 5), np.uint8), iterations=1)
-    cv2.imshow('image', thresh1)
-    cv2.waitKey(0)
-    thresh1 = cv2.erode(thresh1, np.ones((5, 5), np.uint8), iterations=1)
-    cv2.imshow('image', thresh1)
-    cv2.waitKey(0)
-    width = int(thresh1.shape[1] * 50 / 100)
-    height = int(thresh1.shape[0] * 50 / 100)
-    dim = (width, height)
-    thresh1 = cv2.resize(thresh1, dim, interpolation=cv2.INTER_AREA)
-    cv2.imshow('image', thresh1)
-    cv2.waitKey(0)
-    return thresh1
+if __name__ == "__main__":
+    import sys
+    from PIL import Image
+    import os
+    import time
 
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
 
-def detection_mot(tresholedimg):
-    mots = (pytesseract.image_to_string(tresholedimg,
-                                        config='--psm 6 -l fra --oem 0 -c tessedit_char_whitelist=ABCDEFGHIJKL'
-                                               'MNOPQRSTUVWXYZ'))
-    mots = mots.replace(' ', '')
-    results = []
-    for line in mots.split('\n'): results.append(line.strip())
-    results = list(filter(None, results))
-    return results
-    # return [str(result) for result in results if len(result) > 1]
-
-
-def score_mot(letterlist, word, dico):
-    score = 0
-    lst = []
-    if word in dico:
-        word = [i.upper() for i in word]
-        for letters in word:
-            lst.append(letters)
-        for i in lst:
-            for j in letterlist:
-                if i == j[0]:
-                    score = score + j[1]
-        return score
-    return 0
-
-
-def delete_multiple_element(list_object, indices):
-    indices = sorted(indices, reverse=True)
-    for idx in indices:
-        if idx < len(list_object):
-            list_object.pop(idx)
-
-
-def ajoutdesindicesencommun(list_n):
-    listIndex = [[] for i in range(len(list_n))]
-    listMot = []
-    i = 0
-    for j, k in enumerate(list_n):
-
-        if len(k) == 1:
-            listIndex[i].append(j)
-            i -= 1
-        i += 1
-
-    listIndex = [x for x in listIndex if len(x) > 0]
-
-    listIndexNew = [-1 for x in listIndex]
-
-    for k, i in enumerate(listIndex):
-        mot = ''
-        for j in i:
-            mot += list_n[j]
-            if listIndexNew[k] == -1:
-                listIndexNew[k] = j
-        listMot.append(mot)
-
-    k = 0
-    p = 0
-    nindexlist = []
-    for i, j in zip(listIndexNew, listMot):
-        nindexlist = nindexlist + [i - p]
-        if list_n[i - p] != j:
-            list_n[i - p] = j
-            for u in range(1, len(listIndex[k])):
-                list_n.remove(list_n[listIndex[k][u] - p])
-                p += 1
-
-        k += 1
-
-    return list_n, nindexlist
-
-
-def demand_letter_before(mot, listletters):
-    while True:
-        letter = input("Quelle est la premiere lettre de " + mot + '? (si aucune appuyez sur entrée)')
-        letter = letter.upper()
-        if letter in listletters or letter == '':
-            break
-    created_word = letter + mot
-    return created_word
-
-
-def demand_letter_after(mot, listletters):
-    while True:
-        letter = input("Quelle est la derniere lettre de " + mot + '? (si aucune appuyez sur entrée)')
-        letter = letter.upper()
-        if letter in listletters or letter == '':
-            break
-    created_word = mot + letter
-    return created_word
-
-
-def demand_letter_both(mot, listletters_av, listletters_ap):
-    while True:
-        letter_av = input("Quelle est la premiere lettre de " + mot + '? (si aucune appuyez sur entrée)')
-        letter_av = letter_av.upper()
-        if letter_av in listletters_av or letter_av == '':
-            break
-    while True:
-        letter_ap = input("Quelle est la derniere lettre de " + letter_av + mot + '? (si aucune appuyez sur entrée)')
-        letter_ap = letter_ap.upper()
-        if letter_ap in listletters_ap or letter_ap == '':
-            break
-    created_word = letter_av + mot + letter_ap
-    print(created_word)
-    return created_word
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    listLetters = [['A', 1], ['B', 3], ['C', 3], ['D', 2], ['E', 1], ['F', 4], ['G', 2], ['H', 4], ['I', 1], ['J', 8],
-                   ['K', 5], ['L', 1], ['M', 3], ['N', 1], ['O', 1], ['P', 3], ['Q', 10], ['R', 1], ['S', 1], ['T', 1],
-                   ['U', 1], ['V', 4], ['W', 4], ['X', 8], ['Y', 4], ['Z', 10]]
-    scoreA = 0
-    scoreB = 0
-    nbTours = 0
-    listWords = open(r"ressources\\ods6.txt").read().splitlines()
-    previousList = []
-    new = []
-    for i in range(10, 17):
-        nbTours += 1
-        path = fr'ressources\\IMG_2_{str(i)}.jpg'
-        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-        thresholdedImg = traitement_images(img)
-        resultList = detection_mot(thresholdedImg)
-        print("result: ", resultList)
-        res, resIndex = ajoutdesindicesencommun(resultList)
-        for i in resIndex:
-            if i == 0:
-                res[i] = demand_letter_after(res[i], res[i+1])
-            elif i == len(res)-1:
-                res[i] = demand_letter_before(res[i], res[i-1])
-            else:
-                res[i] = demand_letter_both(res[i], res[i-1], res[i+1])
-        print(res)
-        for j in res:
-            score = score_mot(listLetters, j, listWords)
-            print("mot: ", j, "score: ", score)
-        if nbTours > 1:
-            new = [ele for ele in res]
-            for a in previousList:
-                if a in res:
-                    new.remove(a)
-        previousList = res
-        print(new, "new")
+    sys.exit(app.exec_())
